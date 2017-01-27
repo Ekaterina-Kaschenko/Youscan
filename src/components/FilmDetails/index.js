5ee0310982b9c10c2f55248c66d25c6e2f8fbeed
Ekaterina Kashchenko <katikate123@gmail.com>
2017-01-27 19:12:24 +0200

    added card for cheched films

--- src/components/FilmDetails/index.js
+++ src/components/FilmDetails/index.js
@@ -13,20 +13,39 @@ export default class FilmDetails extends React.Component {
   }
 
   componentWillMount() {
-    api.getDetails(142).then((res) => {
+    // let currentFilm = this.state.film.id;
+    api.getDetails(134).then((res) => {
       this.setState({
         film: res
       });
     })
   }
 
+  getDetailsFilm(id) {
+    console.log('film', this.state.film.title);
+    return this.state.film.id;
+  }
+
   render() {
-    console.log('state = ', this.state.film);
+    console.log('state = ', this.state.film.id, this.props);
     return (
-      <div key={this.state.film.id}>
-        <div>{this.state.film.title}</div>
-        <img src={this.state.film.backdrop_path} alt='film' />
-        <div>{this.state.film.overview}</div>
+      <div key={this.state.film.id} 
+           className={styles['item-card']} 
+           onClick={ () => {showFilmWithID(film.id) }} >
+        <h3 className={styles.title}>{this.state.film.title}</h3>
+         <div className={styles.content}>
+          <div className={styles.profile}>
+            <div className={styles['profile__img']}>
+              <img src={this.state.film.backdrop_path} alt='film' />
+            </div>
+            <div className={styles.rating}>
+              rating
+            </div>
+          </div>
+          <div className={styles.info}>
+            <div className={styles.description}>{this.state.film.overview}</div>
+          </div>
+        </div>
       </div>
     );
   }
