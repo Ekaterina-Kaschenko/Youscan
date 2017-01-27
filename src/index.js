8b4066d5d0d3ed946adcf18c385997cc500cf58b
Ekaterina Kashchenko <katikate123@gmail.com>
2017-01-27 19:16:35 +0200

    added route for main app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 // import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React    from 'react';
+import {Router, Route, hashHistory} from 'react-router';
 
 import App   from './containers/App';
+import FilmDetails  from './components/FilmDetails';
+
 // import store from './store';
 
 import './index.html';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <Route path='/details' component={FilmDetails} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
 
