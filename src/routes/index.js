9b5c2e7b69daa7e1e6c151ae7e0a73f737db116d
Ekaterina Kashchenko <katikate123@gmail.com>
2017-01-27 19:13:19 +0200

    added routing

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import {Router, Route, hashHistory} from 'react-router';
+
+// import App from '../containers/App';
+
+// ReactDom.ren
