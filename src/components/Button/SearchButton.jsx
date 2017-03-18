c75817f8cea6056add72a0c9f4871d3dbeb44fee
Ekaterina-Kaschenko <ekaterina_kashchenko@yahoo.com>
2017-03-18 09:38:05 +0200

    fixing search button

--- src/components/Button/SearchButton.jsx
+++ src/components/Button/SearchButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
 import Button from './Button'
 import styles from './styles.css';
 
 class SearchButton extends React.Component {
-
+  
   render(){
+    console.log(this.props)
     return (
       <Button className={ styles['button-search'] }>
-         {this.props.children}
+        {this.props.children}
       </Button>
     )
       
