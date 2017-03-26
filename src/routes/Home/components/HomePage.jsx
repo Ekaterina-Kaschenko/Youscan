ea6c452d20191f1955a1f75138b47caa6299fe23
Ekaterina-Kaschenko <ekaterina_kashchenko@yahoo.com>
2017-03-27 02:16:57 +0300

    added routes

--- src/routes/Home/components/HomePage.jsx
+++ src/routes/Home/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import styles from './styles.css';
-
+import Grid from '../../../components/Grid';
 const HomePage = () => {
   return (
     <Grid className={ styles.container }> 
