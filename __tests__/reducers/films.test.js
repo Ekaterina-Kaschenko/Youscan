4c58e93204e1c1f07a615be61529b813ae9c633c
Ekaterina Kashchenko <ekaterina_kashchenko@yahoo.com>
2018-01-06 19:58:18 +0200

    reducers done

--- __tests__/reducers/films.test.js
+++ __tests__/reducers/films.test.js
@@ -15,11 +15,86 @@ describe('Application reducer', () => {
 
     const mutated = filmsReducers(initialState, action)
 
-    expect(mutated)
-      .to.have.property('isFrontVisible')
-      .that.is.a('boolean')
-      .and.equal(true)
+    expect(mutated).toHaveProperty('films', films)
+  })
 
-   console.log(mutated)
+  it('should load genres on LOAD_GENRE_FILMS_SUCCESS action', () => {
+    const films = [{
+      name: 'Home Along',
+      genres: 'Family'
+    }]
+
+    const action = {
+      type: ActionTypes.LOAD_GENRE_FILMS_SUCCESS,
+      payload: { films }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+
+    expect(mutated).toHaveProperty('films', films)
+  })
+
+  it('should load genres on LOAD_FILM_DETAILS action', () => {
+    const id = 11111
+    const action = {
+      type: ActionTypes.LOAD_FILM_DETAILS,
+      payload: { id }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+
+    expect(mutated).toHaveProperty('film', null)
+  })
+
+  it('should load genres on LOAD_FILM_DETAILS_SUCCESS action', () => {
+    const film = {
+      name: 'Home Along',
+      genres: 'Family'
+    }
+    const action = {
+      type: ActionTypes.LOAD_FILM_DETAILS_SUCCESS,
+      payload: { film }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+
+    expect(mutated).toHaveProperty('film', film)
+  })
+
+  it('should load genres on LOAD_GENRES_SUCCESS action', () => {
+     const genres = ['Drama', 'Comedy', 'History']
+     const action = {
+      type: ActionTypes.LOAD_GENRES_SUCCESS,
+      payload: { genres }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+
+    expect(mutated).toHaveProperty('genres', genres)
+  })
+
+   it('should load genres on SEARCH_FILMS_SUCCESS action', () => {
+    const films = [{
+      name: 'Home Along',
+      genres: 'Family'
+    }]
+     const action = {
+      type: ActionTypes.SEARCH_FILMS_SUCCESS,
+      payload: { films }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+    expect(mutated).toHaveProperty('searchResults', films)
+  })
+
+  it('should load genres on ERROR action', () => {
+    const error = 'ERROR'
+     const action = {
+      type: ActionTypes.ERROR,
+      payload: { error }
+    }
+
+    const mutated = filmsReducers(initialState, action)
+    expect(mutated).toHaveProperty('error', error)
   })
 })
